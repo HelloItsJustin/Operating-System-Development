3f00c0eecdac2b159ace139ae1ceb06915224f17
HelloItsJustin <justinthomasvarghese97@gmail.com>
2025-06-04 10:39:52 +0530

    Added After Removal Of Watcom

--- watcom/watcom/owsetenv.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-echo Open Watcom Build Environment
-export PATH=\\wsl.localhost\Ubuntu\home\justin\OSDev\watcom/binl64:\\wsl.localhost\Ubuntu\home\justin\OSDev\watcom/binl:$PATH
-export INCLUDE=\\wsl.localhost\Ubuntu\home\justin\OSDev\watcom/lh:$INCLUDE
-export WATCOM=\\wsl.localhost\Ubuntu\home\justin\OSDev\watcom
-export EDPATH=\\wsl.localhost\Ubuntu\home\justin\OSDev\watcom/eddat
-export WWINHELP=\\wsl.localhost\Ubuntu\home\justin\OSDev\watcom/binw
-export WIPFC=\\wsl.localhost\Ubuntu\home\justin\OSDev\watcom/wipfc
